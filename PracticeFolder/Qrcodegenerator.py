60be0e37d3f9ab136d7ded1c0ca903f5a2e1dd31
Preetha <preethadk11@gmail.com>
2026-06-26 23:27:22 +0530

    Qr code generator[Learned about the existence of third party libraries]

---
+++ PracticeFolder/Qrcodegenerator.py
@@ -0,0 +1,12 @@
+#qrcodegenerator
+import qrcode
+qr=qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data("https://www.youtube.com")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.show()
